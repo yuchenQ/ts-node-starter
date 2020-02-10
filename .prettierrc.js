9418b0e23cc4dce8eb3548433e124891225ced72
Yuchen Qiao <qiaoycmelbourne@gmail.com>
2020-02-10 21:45:11 +1100

    build: add eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  parser: 'typescript',
+  arrowParens: 'avoid',
+};
